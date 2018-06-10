e7ef59b91ad29abdf77c52a34febc95bc077ef56
Tomohiro <gpuppur@gmail.com>
2018-06-10 09:42:38 +0900

    Added c++ code

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << 
+R"(<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+        Hoge hoge
+    </body>
+</html>
+)" << endl;
+}
